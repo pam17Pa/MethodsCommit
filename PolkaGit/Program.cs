94ec7104ee41e41e36634622c28be4c54e50e8f8
pam17Pa <pa23524184@edu1.pgzaimov.com>
2022-01-12 09:28:52 +0200

    Add sum method

--- PolkaGit/Program.cs
+++ PolkaGit/Program.cs
@@ -17,5 +17,10 @@ namespace PolkaGit
         {
             Console.WriteLine("Hi, lollies <3");
         }
+        public void SumNubers()
+        {
+            int num1 = 4, num2 = 7;
+            Console.WriteLine($"The sum of num1 and num2  is {num1+num2}");
+        }
     }
 }
